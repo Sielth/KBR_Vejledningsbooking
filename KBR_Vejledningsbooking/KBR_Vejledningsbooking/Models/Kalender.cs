c079d331529785588fec5dc07f0833d03ee3bcca
Sielth <sielth@hotmail.it>
2021-09-07 11:18:24 +0200

    Bookingvindue + Kalender klasser

---
+++ KBR_Vejledningsbooking/KBR_Vejledningsbooking/Models/Kalender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KBR_Vejledningsbooking.Models
+{
+    public class Kalender
+    {
+        public int KalenderID;
+        public Bookingvindue bookingvindue;
+        public Hold hold;
+    }
+}
