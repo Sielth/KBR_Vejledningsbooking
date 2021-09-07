24141ff094fdfc2cfe78f49ef08a5c7c792c5cb6
tbni26606 <tbni26606@edu.ucl.dk>
2021-09-07 11:18:41 +0200

    Hold og booking klasse

---
+++ KBR_Vejledningsbooking/KBR_Vejledningsbooking/Models/Booking.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KBR_Vejledningsbooking.Models
+{
+    public class Booking
+    {
+        public int BookingID;
+
+    }
+}
---
+++ KBR_Vejledningsbooking/KBR_Vejledningsbooking/Models/Hold.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KBR_Vejledningsbooking.Models
+{
+    public class Hold
+    {
+        public int HoldID;
+    }
+}
