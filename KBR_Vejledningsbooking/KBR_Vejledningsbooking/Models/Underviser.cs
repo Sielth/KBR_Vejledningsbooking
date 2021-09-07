155f28797a2add77da21f37e9557c594bc700518
Sielth <sielth@hotmail.it>
2021-09-07 11:14:53 +0200

    Underviser klasse

---
+++ KBR_Vejledningsbooking/KBR_Vejledningsbooking/Models/Underviser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KBR_Vejledningsbooking.Models
+{
+    public class Underviser
+    {
+        public int UnderviserID;
+        public Kalender kalender;
+    }
+}
