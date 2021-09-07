c64632c797b8f89bc4e2b1fd7c6eac881dad8bce
tbni26606 <trine.berg.nissen@gmail.com>
2021-09-07 11:22:13 +0200

    Studerende klasse

---
+++ KBR_Vejledningsbooking/KBR_Vejledningsbooking/Models/Studerende.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KBR_Vejledningsbooking.Models
+{
+    public class Studerende
+    {
+    }
+}
